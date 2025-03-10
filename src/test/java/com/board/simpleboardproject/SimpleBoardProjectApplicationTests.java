c3ecfb6f4ae0d308dfbacb2d9ec48564a51415f2
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-11 01:14:37 +0900

    [Init] 프로젝트 초기 설정 프로젝트 초기 설정

---
+++ src/test/java/com/board/simpleboardproject/SimpleBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.simpleboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
