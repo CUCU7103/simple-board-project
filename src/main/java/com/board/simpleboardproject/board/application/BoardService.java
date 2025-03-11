7e3cefac45d502caa17e2a438a49faa46aa40436
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-11 13:50:01 +0900

    [Feat] Board 도메인 기본 설정 진행 Board 도메인 기본 설정 진행

---
+++ src/main/java/com/board/simpleboardproject/board/application/BoardService.java
@@ -0,0 +1,28 @@
+package com.board.simpleboardproject.board.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.board.simpleboardproject.board.dao.BoardRepository;
+import com.board.simpleboardproject.board.domain.Board;
+import com.board.simpleboardproject.board.dto.create.BoardCreateRequestDto;
+import com.board.simpleboardproject.board.dto.create.BoardCreateResponseDto;
+import com.board.simpleboardproject.board.mapper.BoardMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+	private final BoardRepository boardRepository;
+	private final BoardMapper boardMapper;
+
+	@Transactional
+	public BoardCreateResponseDto createBoard(BoardCreateRequestDto dto) {
+
+		Board board = boardRepository.save(boardMapper.toEntity(dto));
+
+		return boardMapper.toDto(board);
+	}
+}
---
+++ src/main/java/com/board/simpleboardproject/board/domain/Board.java
@@ -0,0 +1,82 @@
+package com.board.simpleboardproject.board.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.board.simpleboardproject.global.model.YnCode;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="board")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class Board {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "board_id")
+	private Long boardId;
+
+	@Column(name="board_password")
+	private String boardPassword;
+
+	@Column(name = "user_name")
+	private String username;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "post")
+	private String post;
+
+	@Column(name = "created_at", updatable = false)
+	@CreatedDate
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime createdAt;
+
+	@Column(name = "modified_at")
+	@LastModifiedDate
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime modifiedAt;
+
+	@Column(name = "created_by")
+	private String createdBy;
+
+	@Column(name = "modified_by")
+	private String modifiedBy;
+
+	@Column(name = "deleted_yn")
+	@Enumerated(EnumType.STRING)
+	private YnCode deletedYn;
+
+	@Builder
+	public Board(Long boardId,String boardPassword, String username, String title,
+		String post, String createdBy, String modifiedBy, YnCode deletedYn) {
+		this.boardId = boardId;
+		this.boardPassword = boardPassword;
+		this.username = username;
+		this.title = title;
+		this.post = post;
+		this.createdBy = (createdBy == null) ? username : createdBy;
+		this.modifiedBy = modifiedBy;
+		this.deletedYn = (deletedYn == null) ? YnCode.N : deletedYn;
+	}
+}
