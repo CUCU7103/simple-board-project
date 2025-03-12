910bb568ea22002f6986d14b3a39e5c73c648286
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-12 16:45:42 +0900

    [Feat] User 엔티티 , 리포지토리  등록 User 엔티티와, 리포지토리 등록

---
+++ src/main/java/com/board/simpleboardproject/user/domain/User.java
@@ -0,0 +1,105 @@
+package com.board.simpleboardproject.user.domain;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.board.simpleboardproject.global.model.YnCode;
+import com.board.simpleboardproject.user.model.Role;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails {
+
+	@Id
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "delete_yn")
+	private YnCode deletedYn;
+
+	@Column(name = "created_by")
+	private String createdBy;
+
+	@Column(name = "created_at")
+	@CreatedDate
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime createdAt;
+
+	@Column(name = "modified_by")
+	private String modifiedBy;
+
+	@Column(name = "modified_at")
+	@LastModifiedDate
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime modifiedAt;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	@Builder
+	public User(String username, String password, YnCode deletedYn, String createdBy,
+		String modifiedBy, LocalDateTime modifiedAt, Role role) {
+		this.username = username;
+		this.password = password;
+		this.modifiedAt = modifiedAt;
+		this.createdBy = (createdBy == null) ? username : createdBy;
+		this.modifiedBy = modifiedBy;
+		this.deletedYn = (deletedYn == null) ? YnCode.N : deletedYn;
+		this.role = role;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return YnCode.N.equals(this.deletedYn);
+	}
+}
