307c54b339114856eb253948a21078573ec944fc
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-18 16:18:31 +0900

    [Fix] 누락된 날짜 포매팅 진행 누락된 날짜 포메팅 진행함

--- src/main/java/com/board/simpleboardproject/domain/comment/dto/create/CommentCreateResponseDto.java
+++ src/main/java/com/board/simpleboardproject/domain/comment/dto/create/CommentCreateResponseDto.java
@@ -2,11 +2,14 @@ package com.board.simpleboardproject.domain.comment.dto.create;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 public record CommentCreateResponseDto(@NotBlank long commentId, @NotBlank String post,
-									   @NotBlank LocalDateTime createdAt, @NotBlank String createdBy,@NotBlank String username) {
+									   @NotBlank @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul") LocalDateTime createdAt,
+									   @NotBlank String createdBy, @NotBlank String username) {
 
 	@Builder
 	public CommentCreateResponseDto(@NotBlank long commentId, @NotBlank String post,
