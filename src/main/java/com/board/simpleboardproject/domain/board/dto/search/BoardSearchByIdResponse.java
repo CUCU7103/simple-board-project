108c240b45c8d49af3511d93aff90f9993017ec9
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-18 16:17:08 +0900

    [Fix] Board 조회 시 comment도 조회 가능하도록 수정 연관관계 설정에 맞추어 수정 진행

--- src/main/java/com/board/simpleboardproject/domain/board/dto/search/BoardSearchByIdResponse.java
+++ src/main/java/com/board/simpleboardproject/domain/board/dto/search/BoardSearchByIdResponse.java
@@ -1,20 +1,26 @@
 package com.board.simpleboardproject.domain.board.dto.search;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.board.simpleboardproject.domain.comment.domain.Comment;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Builder;
 
 public record BoardSearchByIdResponse(String title, String username, String post,
-									  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")  LocalDateTime createdAt) {
+									  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")  LocalDateTime createdAt,
+									  List<Comment> comments) {
 
 	@Builder
 	public BoardSearchByIdResponse(String title, String username, String post,
-		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul") LocalDateTime createdAt) {
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul") LocalDateTime createdAt,
+		List<Comment> comments) {
 		this.title = title;
 		this.username = username;
 		this.post = post;
 		this.createdAt = createdAt;
+		this.comments = comments == null ? new ArrayList<>() : comments;
 	}
 }
