47f8b540c8302453c67f911855de6572385f5801
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-18 12:44:37 +0900

    [Fix] 누락된 어노테이션 추가
    Comment Repository에 누락된 어노테이션을 추가

--- src/main/java/com/board/simpleboardproject/domain/comment/dao/CommentRepository.java
+++ src/main/java/com/board/simpleboardproject/domain/comment/dao/CommentRepository.java
@@ -1,8 +1,10 @@
 package com.board.simpleboardproject.domain.comment.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.board.simpleboardproject.domain.comment.domain.Comment;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
