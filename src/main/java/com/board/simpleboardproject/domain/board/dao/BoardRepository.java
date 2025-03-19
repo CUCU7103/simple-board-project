7f072a43b348999d9c5012f04b8dff3b1b7e1211
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-19 23:02:49 +0900

    [Fix] 연관관계 설정 후 조회시 발생한 N + 1 문제 해결 N+1 문제가 발생하여 fetch join 사용하도록 변경

--- src/main/java/com/board/simpleboardproject/domain/board/dao/BoardRepository.java
+++ src/main/java/com/board/simpleboardproject/domain/board/dao/BoardRepository.java
@@ -4,14 +4,19 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.board.simpleboardproject.domain.board.domain.Board;
+import com.board.simpleboardproject.global.model.YnCode;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-	List<Board> findAllByOrderByCreatedAtDesc();
+	@Query("SELECT DISTINCT b FROM Board b LEFT JOIN FETCH b.comments WHERE b.deletedYn = :deletedYn ORDER BY b.createdAt DESC")
+	List<Board> findAllWithCommentsByDeletedYnOrderByCreatedAtDesc(@Param("deletedYn") YnCode deletedYn);
+
 	Optional<Board> findByBoardIdAndBoardPassword(Long boardId,String boardPassword);
 
 }
