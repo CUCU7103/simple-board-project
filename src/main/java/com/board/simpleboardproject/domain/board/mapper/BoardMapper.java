c3cb377fbcfcc571073c7cc641bd5d74dc27ff64
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-20 16:19:01 +0900

    [Fix] 블필요한 코드 제거 mapper에서 불필요한 코드 제거

--- src/main/java/com/board/simpleboardproject/domain/board/mapper/BoardMapper.java
+++ src/main/java/com/board/simpleboardproject/domain/board/mapper/BoardMapper.java
@@ -3,7 +3,6 @@ package com.board.simpleboardproject.domain.board.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import com.board.simpleboardproject.domain.board.domain.Board;
@@ -11,7 +10,6 @@ import com.board.simpleboardproject.domain.board.dto.create.BoardCreateRequestDt
 import com.board.simpleboardproject.domain.board.dto.create.BoardCreateResponseDto;
 import com.board.simpleboardproject.domain.board.dto.search.BoardAllSearchResponse;
 import com.board.simpleboardproject.domain.board.dto.search.BoardSearchByIdResponse;
-import com.board.simpleboardproject.domain.board.dto.update.BoardUpdateRequestDto;
 import com.board.simpleboardproject.domain.board.dto.update.BoardUpdateResponseDto;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -24,9 +22,6 @@ public interface BoardMapper {
 	List<BoardAllSearchResponse> toAllSearchResponseDto(List<Board> entity);
 
 	BoardSearchByIdResponse toSearchByIdResponseDto(Board entity);
-
-	@Mapping(source = "username", target = "modifiedBy")
-	Board toEntity(BoardUpdateRequestDto dto);
 
 	BoardUpdateResponseDto toUpdateResponseDto(Board entity);
 }
